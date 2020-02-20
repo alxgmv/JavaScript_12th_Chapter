dd610e444c468e661d3f34bda3f2e0628df6c6bf
alxgmv <alxgmv@gmail.com>
2020-02-20 16:59:20 +0100

    update_1

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-$('#ball').on('click', function () {
-  let ball = $('#ball');
-  ball.toggleClass('ball_clicked ball');
-  // ball.css('left','inherit').toggle;
-  // alert('Гооол!');
+$( '#ball' ).on( 'click', function () {
+  let ball = $( '#ball' );
+  let className = $( '#ball' ).attr('class')
+  if (className = 'ball') {
+    ball.switchClass( 'ball','ball_clicked', 100, 'easeInOutQuad' );
+    ball.css({ 'top': Math.random()*($('.field').height() - ball.height()) });
+  }
+  if (className = 'ball_clicked') {
+    ball.switchClass( 'ball_clicked','ball', 100, 'easeInOutQuad' );
+    ball.css({ 'top': Math.random()*($('.field').height() - ball.height()) });
+  }
 });
