8e1fb5c536d9740bae9a5442dc733901daf72e0e
alxgmv <alxgmv@gmail.com>
2020-02-24 10:36:41 +0100

    update_2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $( '#ball' ).on( 'click', function () {
   let ball = $( '#ball' );
   let className = $( '#ball' ).attr('class')
-  if (className = 'ball') {
-    ball.switchClass( 'ball','ball_clicked', 100, 'easeInOutQuad' );
+  if (className == 'ball') {
+    ball.switchClass( 'ball','ball_clicked', 500, 'easeInOutQuad' );
+    ball.css({ 'top': Math.random()*($('.field').height() - ball.height()) });
+
+  } else {
+    ball.switchClass( 'ball_clicked','ball', 500, 'easeInOutQuad' );
     ball.css({ 'top': Math.random()*($('.field').height() - ball.height()) });
   }
-  if (className = 'ball_clicked') {
-    ball.switchClass( 'ball_clicked','ball', 100, 'easeInOutQuad' );
-    ball.css({ 'top': Math.random()*($('.field').height() - ball.height()) });
-  }
+  alert('Goal!');
 });
